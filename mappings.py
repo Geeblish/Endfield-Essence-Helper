ad7cbec749071677e787afcca788b9ee54b107b9
geeblish <60635068+Geeblish@users.noreply.github.com>
2026-02-16 16:55:13 -0800

    Remove duplicate entries in mappings.py

--- mappings.py
+++ mappings.py
@@ -7,18 +7,18 @@ STAT_MAPPING: dict[str, list[str]] = {
     "MAIN_ATTRIBUTE_BOOST": ["Main Attribute Boost"],
 
     # Numeric / damage
-    "ATTACK_BOOST": ["ATK Boost", "Attack% Boost"],
+    "ATTACK_BOOST": ["ATK Boost", "Attack Boost"],
     "HP_BOOST": ["HP Boost"],
     "PHYSICAL_DMG_BOOST": ["Physical DMG Boost"],
     "HEAT_DMG_BOOST": ["Heat DMG Boost"],
     "ELECTRIC_DMG_BOOST": ["Electric DMG Boost", "Electric DMG"],
     "CRYO_DMG_BOOST": ["Cryo DMG Boost"],
     "NATURE_DMG_BOOST": ["Nature DMG Boost"],
-    "CRITICAL_RATE_BOOST": ["Critical Rate Boost", "CRIT Rate Boost"],
+    "CRITICAL_RATE_BOOST": ["Critical Rate Boost"],
     "ORIGINIUM_ARTS_BOOST": ["Originium Arts Boost"],
     "ULTIMATE_GAIN_BOOST": ["Ultimate Gain Boost"],
     "ARTS_DMG_BOOST": ["Arts DMG Boost", "Arts Intensity Boost"],
-    "TREATMENT_EFFICIENCY_BOOST": ["Treatment Efficiency Boost", "Treatment Bonus"],
+    "TREATMENT_EFFICIENCY_BOOST": ["Treatment Efficiency Boost"],
 
     # Passive / keyword
     "ASSAULT": ["Assault"],
